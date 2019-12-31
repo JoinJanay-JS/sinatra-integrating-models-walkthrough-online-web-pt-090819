09c8a0c09848f2374e96506f792e16ba8e3bb63d
Janay Bailey <tjanay87@gmail.com>
2019-12-31 19:40:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,10 +6,12 @@ class App < Sinatra::Base
 get '/' do
   erb :index
 end
+end 
 
 post '/' do
   text_from_user = params[:user_text]
   @analyzed_text = TextAnalyzer.new(text_from_user)
   erb :results
 end
-
+end 
+end 
