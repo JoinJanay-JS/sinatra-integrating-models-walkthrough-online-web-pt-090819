26f37ef59a012e7b0eec65f6ea208ba852a05fcf
Janay Bailey <tjanay87@gmail.com>
2019-12-31 19:42:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,17 +1,14 @@
 require_relative 'config/environment'
-require_relative 'models/textanalyzer.rb'
+require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
-
-get '/' do
-  erb :index
+  get '/' do
+    erb :index
   end
-end 
 
-post '/' do
-  text_from_user = params[:user_text]
-  @analyzed_text = TextAnalyzer.new(text_from_user)
-  erb :results
-    end
-  end 
+  post '/' do
+    text_from_user = params[:user_text]
 
+    erb :results
+  end
+end
